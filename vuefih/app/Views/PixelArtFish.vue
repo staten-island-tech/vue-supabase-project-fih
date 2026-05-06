b9b485a8f554a2bddcef200451fb02ffab40710c
Lunacywonders <Isaacn9394@gmail.com>
2026-05-06 12:30:58 -0400

    asd

--- vuefih/app/Views/PixelArtFish.vue
+++ vuefih/app/Views/PixelArtFish.vue
@@ -1,7 +1,19 @@
 <template>
-    <div>
-
-    </div>
+<div id="colorPicker"></div>
+<div>
+<script src="https://cdn.jsdelivr.net/npm/@jaames/iro@5"></script>
+<script>
+  const colorPicker = new iro.ColorPicker("#colorPicker", {
+    width: 200,       
+    color: "#ff0000", 
+    borderWidth: 1,
+    borderColor: "#fff"
+  });
+  colorPicker.on("color:change", function(color) {
+    console.log(color.hexString); 
+  });
+  </script>
+</div>
 </template>
 
 <script setup>
