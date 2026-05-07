c0eb93662336da6de7e5048e6141a7a2559a521e
Lunacywonders <Isaacn9394@gmail.com>
2026-05-07 12:31:25 -0400

    asd

--- vuefih/app/Router/index.js
+++
@@ -1,7 +0,0 @@
-const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    
-  ]
-})
-
--- vuefih/app/app.vue
+++ vuefih/app/app.vue
@@ -1,6 +1,13 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <RouterView/>
   </div>
 </template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
