16380c47b3cb617c31004a498d533b81f235c6f4
Andy-Zhang7223 <andyz127@nycstudents.net>
2026-05-20 11:48:36 -0400

    jadsdfjk

---
+++ vuefih/app/Components/signIn.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h1>Glad To Have You Here!</h1>
+        <input type="text" id= "Email" name="New email" required size="10">
+        <input type="text" id= "Password" name="New Password" required size="10">
+        <router-link :to="Aquarium">
+            <button v-if="Email !== null && Password !== null">To Your Aquarium!</button>
+        </router-link>
+    </div>
+</template>
+
+<script setup lang="ts">
+import Aquarium from './Aquarium.vue';
+
+const Email = document.querySelector("Email")
+const Password = document.querySelector("Password")
+</script>
+
+<style scoped>
+
+</style>
---
+++ vuefih/app/Views/logIn.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>Welcome!</h1>
+    <h2>Please Log In</h2>
+    <input type="text" id="Email" name="Email" required size="10">
+    <input type="text" id="Password" name="Password" required size="10">
+    <router-link :to="Aquarium"><!-- Here Put Something to link tho the Aquarium -->
+      <button v-if="Email !== null && Password !== null">To Your Aquarium!</button>
+    </router-link>
+    <h2>New Here?</h2>
+    <router-link :to="SignIn">
+    <button>Sign In</button>
+    </router-link>
+  </div>
+</template>
+
+<script setup lang="ts">
+import Aquarium from '~/Components/Aquarium.vue';
+import SignIn from '~/Components/signIn.vue';
+
+const Email = document.querySelector("Email")
+const Password = document.querySelector("Password")
+</script>
+
+<style>
+
+</style>
