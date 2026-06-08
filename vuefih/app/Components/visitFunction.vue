c825ffcd703333028babf630b58b97ac87fad435
Andy-Zhang7223 <andyz127@nycstudents.net>
2026-06-08 12:31:04 -0400

    6/8/2026

---
+++ vuefih/app/Components/visitFunction.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <h1>Give Me Their ID!(Can be seen in the URL)</h1>
+        <input type="text" v-model="id"/>
+        <button @click="visitAquarium">Visit Aquarium</button>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import { useRouter } from 'vue-router';
+
+const id = ref('');
+let errormessage = ref('');
+const router = useRouter();
+
+async function visitAquarium() {
+    if (!id.value) {
+        errormessage.value = 'Please enter a valid ID.';
+        return;
+    }
+    else {
+        await router.push(`/aquarium/${id.value}`);
+    }
+}
+
+</script>
+
+<style scoped>
+
+</style>
--- vuefih/app/Pages/Aquarium.vue
+++ vuefih/app/Pages/Aquarium.vue
@@ -1,11 +1,20 @@
 <template>
     <div>
         <img src="https://media.istockphoto.com/id/500739571/photo/aquascaping-of-the-planted-aquarium.jpg?s=612x612&w=0&k=20&c=YCij36GhbrEIid6LmlNT7ak3WBgYTMpGF6bI-6cpjR4=" alt="Aquarium" />
+        <button @click="toggleVisitFunction">Visit Other Aquariums!</button>
+        <VisitFunction v-show="showVisitFunction"/>
     </div>
 </template>
 
 <script setup>
+import VisitFunction from '~/Components/visitFunction.vue';
+import { ref } from 'vue';
 
+const showVisitFunction = ref(false);
+
+function toggleVisitFunction() {
+    showVisitFunction.value = !showVisitFunction.value;
+}
 </script>
 
 <style scoped>
